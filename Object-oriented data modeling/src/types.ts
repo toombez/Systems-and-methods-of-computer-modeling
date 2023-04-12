324e50c4ed4c24b09c1dbbd67db52d1c18277614
Timur <timurtokaev.toombez@gmail.com>
2023-04-13 01:46:37 +0300

    Add first solution

--- Object-oriented data modeling/src/types.ts
+++ Object-oriented data modeling/src/types.ts
@@ -1,63 +1,63 @@
-/**
- * Allowed map elements
- */
-export type MapElementType = 'POINT' | 'POLYGONE' | 'LINE'
+// /**
+//  * Allowed map elements
+//  */
+// export type MapElementType = 'POINT' | 'POLYGONE' | 'LINE'
 
-/**
- * Position on map using array [longitude, lattitude]
- */
-type MapPointArray = [string, string]
+// /**
+//  * Position on map using array [longitude, lattitude]
+//  */
+// type MapPointArray = [string, string]
 
-/**
- * Position on map using object
- */
-type MapPointObject = {
-    longitude: string
-    lattitude: string
-}
+// /**
+//  * Position on map using object
+//  */
+// type MapPointObject = {
+//     longitude: string
+//     lattitude: string
+// }
 
-/**
- * Map point position
- */
-export type MapPoint = MapPointObject | MapPointArray
+// /**
+//  * Map point position
+//  */
+// export type MapPoint = MapPointObject | MapPointArray
 
-export interface MapElementMeta {
-    elementType: MapElementType
-}
+// export interface MapElementMeta {
+//     elementType: MapElementType
+// }
 
-/**
- * Interface for abstract map element
- */
-export interface MapElement {
-    meta: MapElementMeta
-}
+// /**
+//  * Interface for abstract map element
+//  */
+// export interface MapElement {
+//     meta: MapElementMeta
+// }
 
-/**
- * Interface for map `point` element
- */
-export interface MapPointElement extends MapElement {
-    /**
-     * Element position on map
-     */
-    point: MapPoint
-}
+// /**
+//  * Interface for map `point` element
+//  */
+// export interface MapPointElement extends MapElement {
+//     /**
+//      * Element position on map
+//      */
+//     point: MapPoint
+// }
 
-/**
- * Interface for map `line` element
- */
-export interface MapLineElement extends MapElement {
-    /**
-     * Element line position points on map
-     */
-    points: MapPoint[]
-}
+// /**
+//  * Interface for map `line` element
+//  */
+// export interface MapLineElement extends MapElement {
+//     /**
+//      * Element line position points on map
+//      */
+//     points: MapPoint[]
+// }
 
-/**
- * Interface for map `polygone` element
- */
-export interface MapPolygoneElement extends MapElement {
-    /**
-     * Element polygone position points on map
-     */
-    points: MapPoint[]
-}
+// /**
+//  * Interface for map `polygone` element
+//  */
+// export interface MapPolygoneElement extends MapElement {
+//     /**
+//      * Element polygone position points on map
+//      */
+//     points: MapPoint[]
+// }
