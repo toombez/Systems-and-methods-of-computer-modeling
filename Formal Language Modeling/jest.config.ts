786c460fc0edb9757ee1619252832bcc3105f5bd
Timur <timurtokaev.toombez@gmail.com>
2023-04-28 22:20:10 +0300

    Integrate jest

--- Formal Language Modeling/jest.config.ts
+++ Formal Language Modeling/jest.config.ts
@@ -1,8 +1,12 @@
-import { JestConfigWithTsJest } from 'ts-jest'
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
 
 const config: JestConfigWithTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    roots: ['<rootDir>'],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 }
 
 export default config
