8259b6a1b21f8ab60ef3c628c4d4d359d4dfcac8
Timur <timurtokaev.toombez@gmail.com>
2023-04-29 00:08:17 +0300

    Add tape

--- Formal Language Modeling/src/index.ts
+++ Formal Language Modeling/src/index.ts
@@ -3,13 +3,12 @@ import TransitionFactory from '@/structures/TransitionFactory'
 import Transition from '@/structures/Transition'
 import State from '@/structures/State'
 import Alphabet from './structures/Alphabet'
+import Tape from './structures/Tape'
 
 const factory = new TransitionFactory()
 
-const tape = '0101_1110_0101'.split('')
-
 const tm = new TuringMachine(
-    tape,
+    Tape.fromString('0101_1110_0101'),
     new Alphabet(['0', '1', '_']),
     [
         new State([
