b056b0172f7db845f7bc521c863688c091ac567b
Timur <timurtokaev.toombez@gmail.com>
2023-04-29 21:24:53 +0300

    Add alphabets

--- Formal Language Modeling/src/types.ts
+++ Formal Language Modeling/src/types.ts
@@ -1,4 +1,5 @@
 import TuringMachine from '@/structures/TuringMachine'
+import { BINARY_ALPHABET, ENGLISH_ALPHABET, RUSSIAN_ALPHABET } from './constants'
 
 /**
  * Any transition handler
@@ -9,3 +10,23 @@ export type TransitionHandler = (machine: TuringMachine) => void
  * Move transition handler direction
  */
 export type MoveTransitionDirection = 'LEFT' | 'RIGHT'
+
+/**
+ * Utility type for alphabet from readonly array
+ */
+export type Alphabet<T extends ReadonlyArray<string>> = T[number]
+
+/**
+ * Binary alphabet union type
+ */
+export type BinaryAlphabet = Alphabet<typeof BINARY_ALPHABET>
+
+/**
+ * English alphabet union type
+ */
+export type EnglishAlphaber = Alphabet<typeof ENGLISH_ALPHABET>
+
+/**
+ * Russian alphabet union type
+ */
+export type RussianAlphabet = Alphabet<typeof RUSSIAN_ALPHABET>
