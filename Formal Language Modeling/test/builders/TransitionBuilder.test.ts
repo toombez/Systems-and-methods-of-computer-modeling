c658c3d0edbb31b4a6852fc27833bddf125ff747
Timur <timurtokaev.toombez@gmail.com>
2023-04-29 13:51:10 +0300

    Add transition builder tests

---
+++ Formal Language Modeling/test/builders/TransitionBuilder.test.ts
@@ -0,0 +1,31 @@
+import TransitionBuilder from '@builders/TransitionBuilder'
+import TransitionHandlerFactory from '@factories/TransitionHandlerFactory'
+import { describe, expect, test } from '@jest/globals'
+
+const handlersFactory = new TransitionHandlerFactory()
+
+describe('Transition builder module', () => {
+    test('should contain all passed handlers', () => {
+        const builder = new TransitionBuilder('1')
+        const handlers = [
+            handlersFactory.createWriteTransition('0'),
+            handlersFactory.createRightMoveTransition()
+        ]
+
+        const transition = builder
+            .addHandler(handlers[0])
+            .addHandler(handlers[1])
+            .build()
+
+        handlers.forEach((handler) => expect(transition.handlers).toContain(handler))
+    })
+
+    test('should build without errors', () => {
+        const builder = new TransitionBuilder('1')
+
+        expect(() => builder
+            .addHandler(handlersFactory.createMoveTransition('RIGHT'))
+            .build()
+        ).not.toThrow()
+    })
+})
