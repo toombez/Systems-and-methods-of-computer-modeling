ec020bcbbd6f64bb63d96a1a5b1986e7fa6bd716
Timur <timurtokaev.toombez@gmail.com>
2023-04-30 00:02:46 +0300

    Refactor alphabet

--- Formal Language Modeling/src/structures/Alphabet.ts
+++ Formal Language Modeling/src/structures/Alphabet.ts
@@ -1,16 +1,24 @@
+import { DEFAULT_BLANK_SYMBOL } from '@constants'
+import AlphabetBuilder from '@builders/AlphabetBuilder'
+
 /**
  * Is a finite, non-empty set of tape alphabet symbols
  */
 class Alphabet extends Set<string> {
     /**
-     * Default blank symbol for alphabet
+     * Alphabet builder
      */
-    public static readonly DEFAULT_BLANK_SYMBOL = '_'
+    public static readonly Builder = AlphabetBuilder
+
+    /**
+     * Default alphabet blank symbol
+     */
+    public static readonly DEFAULT_BLANK_SYMBOL = DEFAULT_BLANK_SYMBOL
 
     /**
      * Private blank symbol
      */
-    private _blankSymbol!: string
+    private _blankSymbol: string = DEFAULT_BLANK_SYMBOL
 
     /**
      * Create new alphabet
@@ -18,19 +26,18 @@ class Alphabet extends Set<string> {
      * @param symbols alphabet symbols
      * @param blankSymbol blank symbol
      */
-    public constructor(
-        symbols: readonly string[],
-        blankSymbol?: string,
+    constructor(
+        symbols: Iterable<string>,
+        blankSymbol: string = DEFAULT_BLANK_SYMBOL,
     ) {
-        super(symbols)
-
-        this.blankSymbol = blankSymbol || Alphabet.DEFAULT_BLANK_SYMBOL
+        super([...symbols, blankSymbol])
+        this.blankSymbol = blankSymbol
     }
 
     /**
      * Alphabet blank symbol
      */
-    public get blankSymbol() {
+    public get blankSymbol(): string {
         return this._blankSymbol
     }
 
@@ -38,24 +45,50 @@ class Alphabet extends Set<string> {
      * Set blank symbol and remove previous from alphabet symbols
      */
     public set blankSymbol(symbol: string) {
-        const previousBlankSymbol = this.blankSymbol
-        this._blankSymbol = symbol
+        if (this.isBlankSymbol(symbol)) {
+            return
+        }
 
-        if (previousBlankSymbol !== symbol) {
-            this.delete(this.blankSymbol)
-
+        if (!this.has(symbol)) {
             this.add(symbol)
         }
 
+        const previousBlankSymbol = this.blankSymbol
+        this._blankSymbol = symbol
+        this.delete(previousBlankSymbol)
     }
 
     /**
      * Clear alphabet and add inside blank symbol
      */
     public clear(): void {
-        super.clear()
+        const blankSymbol = this.blankSymbol
 
-        this.add(Alphabet.DEFAULT_BLANK_SYMBOL)
+        super.clear()
+        super.add(blankSymbol)
+    }
+
+    /**
+     * Delete symbol from alphabet
+     * @param symbol symbol to delete
+     * @returns Returns true if an element in the Set existed and has been
+     * removed, or false if the element does not exist.
+     */
+    public delete(symbol: string): boolean {
+        if (this.isBlankSymbol(symbol)) {
+            throw new Error(`Cannot delete blank symbol "${this.blankSymbol}"`)
+        }
+
+        return super.delete(symbol)
+    }
+
+    /**
+     * Check symbol is blank
+     * @param symbol symbol to check
+     * @returns is blank symbol
+     */
+    protected isBlankSymbol(symbol: string): boolean {
+        return symbol === this.blankSymbol
     }
 }
 
